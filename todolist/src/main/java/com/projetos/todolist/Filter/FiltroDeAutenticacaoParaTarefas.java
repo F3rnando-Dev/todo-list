b29b06d7458c39bf0f76a83d520d2ad3dafb8881
Fernando Santana <fernando_92-@hotmail.com>
2023-10-13 21:50:15 -0300

    feat: add validacao de autenticacao para rota de tarefas

--- todolist/src/main/java/com/projetos/todolist/Filter/FiltroDeAutenticacaoParaTarefas.java
+++ todolist/src/main/java/com/projetos/todolist/Filter/FiltroDeAutenticacaoParaTarefas.java
@@ -18,37 +18,38 @@ public class FiltroDeAutenticacaoParaTarefas extends OncePerRequestFilter {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-
-
-
-
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        var autorizacao = request.getHeader("Authorization");
-        var autorizacaoCripto = autorizacao.substring("Basic".length()).trim();
+        var servletPath = request.getServletPath();
 
-        byte[] autorizacaoBase64 = Base64.getDecoder().decode(autorizacaoCripto);
+        if (servletPath.equals("/tarefas/")) {
+            var autorizacao = request.getHeader("Authorization");
+            var autorizacaoCripto = autorizacao.substring("Basic".length()).trim();
 
-        String autorizacaoString = new String(autorizacaoBase64);
+            byte[] autorizacaoBase64 = Base64.getDecoder().decode(autorizacaoCripto);
 
-        String[] credencial = autorizacaoString.split(":");
-        String loginCredencial = credencial[0];
-        String senhaCredencial = credencial[1];
+            String autorizacaoString = new String(autorizacaoBase64);
+
+            String[] credencial = autorizacaoString.split(":");
+            String loginCredencial = credencial[0];
+            String senhaCredencial = credencial[1];
 
 
-        var usuario = this.usuarioRepository.findByLogin(loginCredencial);
-        if (usuario == null){
-            response.sendError(401, "Usuário sem autorização!");
-        }else{
+            var usuario = this.usuarioRepository.findByLogin(loginCredencial);
+            if (usuario == null) {
+                response.sendError(401, "Usuário sem autorização!");
+            } else {
 
-            var verificacaoSenha = BCrypt.verifyer().verify(senhaCredencial.toCharArray(), usuario.getSenha());
-            if(verificacaoSenha.verified){
-                filterChain.doFilter(request,response);
-            } else{
-
+                var verificacaoSenha = BCrypt.verifyer().verify(senhaCredencial.toCharArray(), usuario.getSenha());
+                if (verificacaoSenha.verified) {
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
             }
+        } else{
+            filterChain.doFilter(request,response);
         }
     }
 }
