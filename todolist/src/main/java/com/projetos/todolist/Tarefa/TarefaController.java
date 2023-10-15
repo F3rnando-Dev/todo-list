2fb754884d57862480b19d2db0f043ab9a3423c5
Fernando Santana <fernando_92-@hotmail.com>
2023-10-14 22:51:38 -0300

    feat: add validacao para datas das tarefas

--- todolist/src/main/java/com/projetos/todolist/Tarefa/TarefaController.java
+++ todolist/src/main/java/com/projetos/todolist/Tarefa/TarefaController.java
@@ -2,11 +2,14 @@ package com.projetos.todolist.Tarefa;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -17,11 +20,22 @@ public class TarefaController {
     private TarefaRepository tarefaRepository;
 
     @PostMapping("/")
-    public TarefaModel criaTarefa(@RequestBody TarefaModel tarefaModel, HttpServletRequest request){
+    public ResponseEntity criaTarefa(@RequestBody TarefaModel tarefaModel, HttpServletRequest request){
         System.out.println("Chegou na controller "+request.getAttribute("idLogin"));
         var idRequest = request.getAttribute("idLogin");
         tarefaModel.setIdLogin((UUID) idRequest);
+
+        var dataAtual = LocalDateTime.now();
+        if(dataAtual.isAfter(tarefaModel.getDataInicio()) || dataAtual.isAfter(tarefaModel.getDataTermino())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início/termino deve ser maior que a atual!");
+        }
+
+        if(tarefaModel.getDataInicio().isAfter(tarefaModel.getDataTermino())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve ser menor que a data de término!");
+        }
+
+
         var tarefa = this.tarefaRepository.save(tarefaModel);
-        return tarefa;
+        return ResponseEntity.status(HttpStatus.OK).body(tarefa);
     }
 }
