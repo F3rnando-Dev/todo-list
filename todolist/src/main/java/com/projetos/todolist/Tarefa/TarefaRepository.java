00a8376bab8f9257e4fe8e68f99154000b639870
Fernando Santana <fernando_92-@hotmail.com>
2023-10-17 12:08:00 -0300

    feat: add lista por id do usuario cadastrado

--- todolist/src/main/java/com/projetos/todolist/Tarefa/TarefaRepository.java
+++ todolist/src/main/java/com/projetos/todolist/Tarefa/TarefaRepository.java
@@ -2,7 +2,10 @@ package com.projetos.todolist.Tarefa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TarefaRepository extends JpaRepository<TarefaModel, UUID> {
+    List<TarefaModel> findByIdLogin(UUID idLogin);
+
 }
